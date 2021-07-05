e7cd8c907fd68d7a64f2d6d7a05cde13a2ea4a99
Jose J. Pardines <info@josejpg.com>
2021-07-05 20:33:29 +0200

    Added test to crops_service and fields_service

--- soil-service/spec/services/crops_service_spec.rb
+++ soil-service/spec/services/crops_service_spec.rb
@@ -1,18 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe CropsService do
+
+  CROP_VALUE_EXISTS = 1
+  CROP_VALUE_NOT_EXISTS = 99
+
   describe '#fetch_all_crops' do
     subject(:fetch_all_crops) { described_class.instance.fetch_all_crops }
 
     it 'returns all crops' do
       expect(fetch_all_crops).to contain_exactly(
-        { value: 1, label: 'Spring Wheat', humus_delta: -2 },
-        { value: 2, label: 'Winter Wheat', humus_delta: -1 },
-        { value: 3, label: 'Red Clover', humus_delta: 2 },
-        { value: 4, label: 'White Clover', humus_delta: 1 },
-        { value: 5, label: 'Broad Bean', humus_delta: 3 },
-        { value: 6, label: 'Oats', humus_delta: 0 }
-      )
+                                   { value: 1, label: 'Spring Wheat', humus_delta: -2 },
+                                   { value: 2, label: 'Winter Wheat', humus_delta: -1 },
+                                   { value: 3, label: 'Red Clover', humus_delta: 2 },
+                                   { value: 4, label: 'White Clover', humus_delta: 1 },
+                                   { value: 5, label: 'Broad Bean', humus_delta: 3 },
+                                   { value: 6, label: 'Oats', humus_delta: 0 }
+                                 )
+    end
+
+    # Fetch a single crop by Value
+    describe '#fetch_field_by_id' do
+      subject(:fetch_crop_by_value) { described_class.instance.fetch_crop_by_value(CROP_VALUE_EXISTS) }
+      subject(:fetch_crop_by_value_error) { described_class.instance.fetch_crop_by_value(CROP_VALUE_NOT_EXISTS) }
+
+      it 'returns crop object' do
+        expect(fetch_crop_by_value).to include(
+                                         value: an_instance_of(Integer),
+                                         label: an_instance_of(String),
+                                         humus_delta: an_instance_of(Integer)
+                                       )
+      end
+
+      it 'has exactly values' do
+        expect(fetch_crop_by_value).to contain_exactly(
+                                         [:humus_delta, -2],
+                                         [:label, 'Spring Wheat'],
+                                         [:value, 1]
+                                       )
+      end
+
+      it 'has not exactly values' do
+        expect(fetch_crop_by_value).to_not contain_exactly(
+                                             [:value, 1],
+                                             [:label, 'Spring Wheat']
+                                           )
+      end
+
+      it 'returns CropsError exception' do
+        expect { fetch_crop_by_value_error }.to raise_error(CropsError::NotFound)
+      end
     end
   end
 end
--- soil-service/spec/services/fields_service_spec.rb
+++ soil-service/spec/services/fields_service_spec.rb
@@ -1,31 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe FieldsService do
+
+  FIELD_ID_EXISTS = 1
+  FIELD_ID_NOT_EXISTS = 99
+
   describe '#fetch_fields' do
     subject(:fetch_fields) { described_class.instance.fetch_fields }
 
     it 'returns fields objects' do
       expect(fetch_fields).to all(
-        include(
-          id: an_instance_of(Integer),
-          name: an_instance_of(String),
-          area: an_instance_of(Float)
-        )
-      )
+                                include(
+                                  id: an_instance_of(Integer),
+                                  name: an_instance_of(String),
+                                  area: an_instance_of(Float)
+                                )
+                              )
     end
 
     it 'has crops for 5 years' do
       fetch_fields.each do |field|
         expect(field[:crops]).to match_array(
-          [
-            include(year: 2020),
-            include(year: 2021),
-            include(year: 2022),
-            include(year: 2023),
-            include(year: 2024),
-          ]
-        )
+                                   [
+                                     include(year: 2020),
+                                     include(year: 2021),
+                                     include(year: 2022),
+                                     include(year: 2023),
+                                     include(year: 2024),
+                                   ]
+                                 )
       end
+    end
+  end
+
+  # Fetch a single field by ID
+  describe '#fetch_field_by_id' do
+    subject(:fetch_field_by_id) { described_class.instance.fetch_field_by_id(FIELD_ID_EXISTS) }
+    subject(:fetch_field_by_id_error) { described_class.instance.fetch_field_by_id(FIELD_ID_NOT_EXISTS) }
+
+    it 'returns field object' do
+      expect(fetch_field_by_id).to include(
+                                     id: an_instance_of(Integer),
+                                     name: an_instance_of(String),
+                                     area: an_instance_of(Float)
+                                   )
+    end
+
+    it 'has crops for 5 years' do
+      expect(fetch_field_by_id[:crops]).to match_array(
+                                             [
+                                               include(year: 2020),
+                                               include(year: 2021),
+                                               include(year: 2022),
+                                               include(year: 2023),
+                                               include(year: 2024),
+                                             ]
+                                           )
+    end
+
+    it 'returns FieldsError exception' do
+      expect { fetch_field_by_id_error }.to raise_error(FieldsError::NotFound)
     end
   end
 end
